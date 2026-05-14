1fc7c371f2fb36051d99fd535476d814fd3af1f9
AntonioScirica <as.scirica@gmail.com>
2026-05-14 22:19:13 +0200

    chore: add Meta facebook-domain-verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://getnearme.it"),
+  other: {
+    "facebook-domain-verification": "3el76s85o30orscaoxt1ceryo0tbki",
+  },
 };
 
 export default function RootLayout({
